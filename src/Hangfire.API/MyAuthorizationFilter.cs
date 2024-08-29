710b42a85ec55ae66d4c107118c0c87c4de23521
maumauagain <amauri.martins.jr@hotmail.com>
2024-08-29 02:15:01 -0300

    Allowing hangfire dashboard visualization for remote access

---
+++ src/Hangfire.API/MyAuthorizationFilter.cs
@@ -0,0 +1,14 @@
+using Hangfire.Dashboard;
+
+public class MyAuthorizationFilter : IDashboardAuthorizationFilter
+{
+	public bool Authorize(DashboardContext context)
+	{
+		// var httpContext = context.GetHttpContext();
+
+		// // Allow all authenticated users to see the Dashboard (potentially dangerous).
+		// return httpContext.User.Identity?.IsAuthenticated ?? false;
+		
+		return true;
+	}
+}
--- src/Hangfire.API/Program.cs
+++ src/Hangfire.API/Program.cs
@@ -11,47 +11,52 @@ builder.Services.AddOpenApi();
 
 var hangFireConnectionString = builder.Configuration.GetConnectionString("HangfireConnection");
 builder.Services.AddHangfire(configuration => configuration
-    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-    .UseSimpleAssemblyNameTypeSerializer()
-    .UseRecommendedSerializerSettings()
-    .UseSqlServerStorage(hangFireConnectionString)
+	.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+	.UseSimpleAssemblyNameTypeSerializer()
+	.UseRecommendedSerializerSettings()
+	.UseSqlServerStorage(hangFireConnectionString)
 );
 builder.Services.AddHangfireServer();
 
 var app = builder.Build();
 
+app.UseAuthorization();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseHangfireDashboard();
-    app.MapOpenApi();
+	app.UseHangfireDashboard("/hangfire", new DashboardOptions 
+	{
+		Authorization = new [] { new MyAuthorizationFilter()}
+	});
+	app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
 app.MapControllers();
+app.MapHangfireDashboard();
 
 RecurringJob.AddOrUpdate(
-    "minha-tarefa-recorrente",
-    () => Console.WriteLine("Hello world from Hangfire Recurring!"),
+	"minha-tarefa-recorrente",
+	() => Console.WriteLine("Hello world from Hangfire Recurring!"),
 Cron.Minutely
 );
 
 BackgroundJob.Enqueue(() => new Testinho().JobDoido());
 BackgroundJob.Schedule(
-    () => Console.WriteLine("Hello world from Hangfire Scheduled!"),
-    TimeSpan.FromMinutes(2)
+	() => Console.WriteLine("Hello world from Hangfire Scheduled!"),
+	TimeSpan.FromMinutes(2)
 );
 
 app.Run();
 
 record Testinho()
 {
-    public async Task JobDoido()
-    {
-        await Task.Delay(TimeSpan.FromSeconds(10));
-        Console.WriteLine("Hello world from Hangfire Enqueue!");
-    }
+	public async Task JobDoido()
+	{
+		await Task.Delay(TimeSpan.FromSeconds(10));
+		Console.WriteLine("Hello world from Hangfire Enqueue!");
+	}
 }
